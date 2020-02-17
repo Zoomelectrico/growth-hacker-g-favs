2196c191f283a59d938c3fc50e830b3242fc808f
Jose Roberto Quevedo <jose.quevedo2011@gmail.com>
2020-02-17 10:39:33 -0400

    /

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 export const endpoint =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://g-favs-backend.herokuapp.com/';
-export const prodEndpoint = 'https://g-favs-backend.herokuapp.com/';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://g-favs-backend.herokuapp.com';
+export const prodEndpoint = 'https://g-favs-backend.herokuapp.com';
 export const perPage = 6;
